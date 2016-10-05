3c6a5431f25b821fad560f0f7ed6d7518b0ef030
tmt <thomas.culdaut@gmail.com>
2016-10-05 23:23:18 +0200

    <BoardGame /> should have a remove button that call onRemove function

--- src/components/BoardGame.js
+++ src/components/BoardGame.js
@@ -1,13 +1,15 @@
 import React, {PropTypes} from 'react';
 
-const BoardGame = ({name}) => (
+const BoardGame = ({name, onRemove}) => (
 <div>
   <h4>{name}</h4>
+  <button onClick={onRemove}>remove</button>
 </div>
 );
 
 BoardGame.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  onRemove: PropTypes.func.isRequired
 };
 
 export default BoardGame;
--- src/components/BoardGame.spec.js
+++ src/components/BoardGame.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
+import sinon from 'sinon';
 import BoardGame from './BoardGame';
 
 export const mockItem = () => ({
-  name: 'Dungeon'
+  name: 'Dungeon Twister'
 });
 
 describe('<BoardGame />', () => {
@@ -15,5 +16,19 @@ describe('<BoardGame />', () => {
     const expected = item.name;
 
     expect(actual).to.equal(expected);
+  });
+});
+
+describe('<BoardGame />', () => {
+  it('should have a remove button that call onRemove function', () => {
+    const item = mockItem();
+    const onRemove = sinon.spy();
+    const wrapper = shallow(<BoardGame {...item} onRemove={onRemove} />);
+    const removeButtonWrapper = wrapper.find('button');
+    const expected = 'remove';
+
+    expect(removeButtonWrapper.text()).to.equal(expected);
+    removeButtonWrapper.simulate('click');
+    expect(onRemove.calledOnce).to.be.true;
   });
 });
