5ba925dbbc1157a784979bf7bfbf4b5233ff9b48
tmt <thomas.culdaut@gmail.com>
2016-10-06 21:38:35 +0200

    createReducer should return a reducer handling someActionType and should return a reducer that return state for not handled actions

---
+++ src/reducers/createReducer.js
@@ -0,0 +1,12 @@
+import R from 'ramda';
+import {getActionType} from './itemsUtils';
+
+export default function createReducer(initialState, handlers) {
+  return function reducer(state = initialState, action) {
+    return R.propOr(
+      R.identity,
+      getActionType(action),
+      handlers
+    )(state, action);
+  };
+}
---
+++ src/reducers/createReducer.spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import createReducer from './createReducer';
+
+describe('createReducer', () => {
+  const handlers = {
+    someActionType: () => 'modified state'
+  };
+  const handledAction = {
+    type: 'someActionType'
+  };
+  const notHandledAction = {
+    type: 'anotherActionType'
+  };
+  const initialState = 'initialState';
+  const reducer = createReducer(initialState, handlers);
+  it('should return a reducer handling someActionType', () => {
+    expect(reducer(initialState, handledAction)).to.equal('modified state');
+  });
+  it('should return a reducer that return state for not handled actions', () => {
+    expect(reducer(initialState, notHandledAction)).to.equal(initialState);
+  });
+});
