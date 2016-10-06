b11874ab2e796f1d44547f6b2bb2a4a9801cfd6c
tmt <thomas.culdaut@gmail.com>
2016-10-06 14:59:42 +0200

    Formating

--- src/reducers/itemsUtils.js
+++ src/reducers/itemsUtils.js
@@ -6,7 +6,10 @@ export const getActionPayload = R.prop('payload');
 
 export const getIdFromActionPayload = R.pipe(getActionPayload, R.prop('id'));
 
-export const getIdAsStringFromActionPayload = R.pipe(getIdFromActionPayload, String);
+export const getIdAsStringFromActionPayload = R.pipe(
+  getIdFromActionPayload,
+   String
+ );
 
 export const deleteItemFromStateByAction = (state, action) => (
   R.omit(getIdAsStringFromActionPayload(action), state)
