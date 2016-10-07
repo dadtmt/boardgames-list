76a15a5a92836724a112b64fafadbb9b1b855eef
tmt <thomas.culdaut@gmail.com>
2016-10-07 15:01:51 +0200

    combine form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import boardGames from './boardGamesReducer'
 import fuelSavings from './fuelSavingsReducer'
 import {routerReducer} from 'react-router-redux'
+import {reducer as form} from 'redux-form'
 
 const rootReducer = combineReducers({
   boardGames,
   fuelSavings,
-  routing: routerReducer
+  routing: routerReducer,
+  form
 })
 
 export default rootReducer
