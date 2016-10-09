b744faf62375630e6596237552b86982ac2f8f8b
tmt <thomas.culdaut@gmail.com>
2016-10-09 17:06:02 +0200

    Test add / remove in browser

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import BoardGameList from '../containers/BoardGameListContainer'
-import AddBoardGameForm from '../components/AddBoardGameForm'
+import AddBoardGame from '../containers/AddBoardGameContainer'
 
 const HomePage = () => {
   return (
     <div>
       <h1>BoardGame List</h1>
-      <AddBoardGameForm handleSubmit={()=>{}}/>
+      <AddBoardGame />
       <BoardGameList />
     </div>
   )
